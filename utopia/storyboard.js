e78d48ae1050a912ae12a48c4830848f66cf57c4
dyesnap <dyesnap@gmail.com>
2024-11-26 09:20:17 +0100

    Committed automatically

---
+++ utopia/storyboard.js
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { Scene, Storyboard } from 'utopia-api'
+import { App } from '../src/app'
+import { Playground } from '../src/playground'
+
+export var storyboard = <Storyboard />
